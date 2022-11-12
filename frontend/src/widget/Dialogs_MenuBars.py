b9610f8552026dd1a4edb9ff5c33aa9543250156
kappakun17 <ren.programing@gmail.com>
2022-11-12 23:53:23 +0900

    enterとescapeによる、実行ボタンや画面閉じを追加実装しました。

--- frontend/src/widget/Dialogs_MenuBars.py
+++ frontend/src/widget/Dialogs_MenuBars.py
@@ -18,6 +18,7 @@ class my_Dialogs_MenuBars(tk.Frame):
         self.db = DB
         self.json = JSON
         self.name = None
+        self.dialog = None
         
     def create_dialog(self, title):
         dialog = tk.Toplevel(self, bg='#fffdf8')
@@ -30,41 +31,55 @@ class my_Dialogs_MenuBars(tk.Frame):
     
     def create_category_menu(self):
         self.name = self.json['name']
-        category_dialog = self.create_dialog("Category Menu")
-        self.categoryMenu = my_CategoryMenu(category_dialog, name=self.name)
+        self.dialog = self.create_dialog("Category Menu")
+        self.categoryMenu = my_CategoryMenu(self.dialog, name=self.name)
         self.categoryMenu.category_add_btn.bind("<Button-1>", partial(self.open_screen, key='category', action='add'))
         self.categoryMenu.folder_add_btn.bind("<Button-1>", partial(self.open_screen, key='folder', action='add'))
         self.categoryMenu.category_rename_btn.bind("<Button-1>", partial(self.open_screen, key='category', action='rename'))
         self.categoryMenu.category_delete_btn.bind("<Button-1>", partial(self.open_screen, key='category', action='delete'))
         
+        self.dialog.bind("<Escape>", self.close_action_screen)
         
-        self.wait_window(category_dialog)
+        self.wait_window(self.dialog)
         
     def create_folder_menu(self):
         self.name = self.json['name']
-        folder_dialog = self.create_dialog("Folder Menu")
-        self.folderMenu = my_FolderMenu(folder_dialog, name=self.name)
+        self.dialog = self.create_dialog("Folder Menu")
+        self.folderMenu = my_FolderMenu(self.dialog, name=self.name)
         self.folderMenu.folder_rename_btn.bind("<Button-1>", partial(self.open_screen, key='folder', action='rename'))
         self.folderMenu.bookmark_add_btn.bind("<Button-1>", partial(self.open_screen, key='bookmark', action='add'))
         self.folderMenu.folder_delete_btn.bind("<Button-1>", partial(self.open_screen, key='folder', action='delete'))
-        self.wait_window(folder_dialog)
+        
+        self.dialog.bind("<Escape>", self.close_action_screen)
+        
+        self.wait_window(self.dialog)
+        
+        
         
     def create_bookmark_menu(self):
         self.name = self.json['name']
-        bookmark_dialog = self.create_dialog("Bookmark Menu")
-        self.bookmarkMenu = my_BookmarkMenu(bookmark_dialog, title=self.name)
+        self.dialog = self.create_dialog("Bookmark Menu")
+        self.bookmarkMenu = my_BookmarkMenu(self.dialog, title=self.name)
         self.bookmarkMenu.bookmark_add_btn.bind("<Button-1>", partial(self.open_screen, key='bookmark', action='add'))
         self.bookmarkMenu.bookmark_edit_btn.bind("<Button-1>", partial(self.open_screen, key='bookmark', action='edit'))
         self.bookmarkMenu.bookmark_delete_btn.bind("<Button-1>", partial(self.open_screen, key='bookmark', action='delete'))
-        self.wait_window(bookmark_dialog)
+        
+        self.dialog.bind("<Escape>", self.close_action_screen)
+        
+        self.wait_window(self.dialog)
+        
 
     def create_settings_menu(self):
         self.name = self.json['name']
-        settings_dialog = self.create_dialog("Settings Menu")
-        self.settingsMenu = my_SettingsMenu(settings_dialog, title=self.name)
+        self.dialog = self.create_dialog("Settings Menu")
+        self.settingsMenu = my_SettingsMenu(self.dialog, title=self.name)
         self.settingsMenu.show_instructions_btn.bind("<Button-1>", partial(self.open_screen, key='settings', action='introduction'))
         self.settingsMenu.initialize_database_btn.bind("<Button-1>", partial(self.open_screen, key='settings', action='initialize_database'))
-        self.wait_window(settings_dialog)
+        
+        self.dialog.bind("<Escape>", self.close_action_screen)
+        
+        self.wait_window(self.dialog)
+        
         
     # key = bookmark, action= add || edit || rename
     def open_screen(self,event, key=None, action=None):
@@ -73,3 +88,7 @@ class my_Dialogs_MenuBars(tk.Frame):
         self.destroy()
         master = self.master
         self.action_screen = my_Dialogs_Actions(master, JSON=self.json, key=key, action=action, DB=self.db, APP=self.app)
+        
+    def close_action_screen(self, event=None, widget=None):
+        if widget is None: return self.dialog.destroy()
+        widget.destroy()
